e2e06b18408fe8c6cba6d07e341d9e275f01c697
Abulegenv <bulegenovabuzarr@gmail.com>
2026-07-15 15:52:44 +0500

    Update Program.cs

--- Project Utily/Program.cs
+++ Project Utily/Program.cs
@@ -8,9 +8,6 @@ namespace Project_Utily
 {
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
